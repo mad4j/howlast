4e483c2e253ac160a240e725a0056fcd519f7321
Daniele Olmisani <daniele.olmisani@gmail.com>
2025-04-12 06:42:27 +0200

    working draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,77 @@
 use proc_macro::TokenStream;
 use quote::quote;
+use syn::{Expr, Token, parse::Parse, parse_macro_input};
 
-/// Time the duration of code snippet, either to stdout or via `tracing`.
-#[proc_macro]
-pub fn howlast(input: TokenStream) -> TokenStream {
-
-    // Convert the input TokenStream to a proc_macro2::TokenStream
-    let block: proc_macro2::token_stream::TokenStream = input.into();
-
-    let output = quote! {
-        {
-            let begin = line!();
-            let start = std::time::Instant::now();
-            let result =
-                #block;
-            let duration: std::time::Duration = start.elapsed();
-            #[cfg(not(feature = "tracing"))]
-            println!("{}:{} took {:?}.", file!(), begin, duration);
-            #[cfg(feature = "tracing")]
-            tracing::trace!("{}:{} took {:?}.", file!(), begin, duration);
-            result
-        }
-    };
-
-    output.into()
+/// A struct to hold the arguments for the macro
+/// The first argument is the variable name to hold the duration.
+/// The second argument is the block of code to be executed.
+/// The `duration_holder` is of type `Expr` to allow any valid Rust expression.
+/// The `_comma` is a token to ensure that the arguments are separated by a comma.
+/// The `code_block` is also of type `Expr` to allow any valid Rust expression.
+struct MacroArgs {
+    duration_holder: Expr,
+    _comma: Token![,],
+    code_block: Expr,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+/// The `Parse` trait is implemented to parse the input TokenStream into our struct.
+impl Parse for MacroArgs {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        Ok(Self {
+            duration_holder: input.parse()?,
+            _comma: input.parse()?,
+            code_block: input.parse()?,
+        })
     }
+}
+
+/// A macro to measure the duration of a code block
+/// and store the result in a variable.
+///
+/// # Examples
+/// ```
+/// use howlast::howlast;
+///
+/// #[test]
+/// fn it_works() {
+///     howlast!(step_duration,
+///         {
+///             let x = 1 + 1;
+///             std::thread::sleep(std::time::Duration::from_secs(1));
+///             x
+///         }
+///     );
+///     print!("{:?}", step_duration);
+/// }
+/// ```
+/// This macro takes two arguments:
+/// 1. A variable name to hold the duration.
+/// 2. A block of code to be executed.
+/// The macro will measure the time taken to execute the block
+/// and store the duration in the variable.
+/// The block of code can be any valid Rust expression.
+/// The duration is measured using `std::time::Instant`.
+/// The macro will return the result of the block of code.
+/// The duration is stored in the variable passed as the first argument.
+/// The variable name must be a valid Rust identifier.
+#[proc_macro]
+pub fn howlast(input: TokenStream) -> TokenStream {
+    // Parse the input TokenStream into our MacroArgs struct
+    let input = parse_macro_input!(input as MacroArgs);
+
+    let duration_holder = &input.duration_holder;
+    let code_block = &input.code_block;
+
+    // Create a new TokenStream with the timing code
+    // and the original block of code
+    let output = quote! {
+        let #duration_holder = std::time::Instant::now();
+        let result =
+                #code_block;
+        let #duration_holder: std::time::Duration = #duration_holder.elapsed();
+        result
+    };
+
+    // return the generated code as a TokenStream
+    output.into()
 }
---
+++ tests/tests.rs
@@ -0,0 +1,16 @@
+
+use howlast::howlast;
+
+#[test]
+fn it_works() {
+    howlast!(step_duration, 
+        { 
+            let x = 1 + 1;
+            std::thread::sleep(std::time::Duration::from_secs(1));
+            x 
+        }
+    );
+
+    print!("{:?}", step_duration);
+}
+
